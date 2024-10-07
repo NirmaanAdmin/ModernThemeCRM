a1de49cfebc52900b493141ba41875246e6ddcb5
Jakirhuseain <jakirhuseain530@gmail.com>
2024-10-07 18:39:53 +0530

    Php 8.2 changes part 2

--- application/vendor/fakerphp/faker/src/Faker/Provider/nl_NL/Company.php
+++ application/vendor/fakerphp/faker/src/Faker/Provider/nl_NL/Company.php
@@ -103,7 +103,7 @@ class Company extends \Faker\Provider\Company
      *
      * @see https://www.belastingdienst.nl/wps/wcm/connect/bldcontentnl/belastingdienst/zakelijk/btw/administratie_bijhouden/btw_nummers_controleren/uw_btw_nummer
      *
-     * @return string VAT Number
+     * @return string GST Number
      */
     public static function vat()
     {
--- modules/mailbox/views/mailbox.php
+++ modules/mailbox/views/mailbox.php
@@ -107,7 +107,7 @@
                                         </li>
                                         <li role="presentation" data-toggle="tooltip" title="" class="tab-separator" data-original-title="<?php echo _l('mailbox_remove_star'); ?>">
                                             <a href="Javascript:void(0)" aria-controls="tab_emails_tracking" role="tab" data-toggle="tab" onclick="update_mass('<?php echo $group; ?>','starred',1);window.location.reload(); return false;">
-                                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                                                <i class="fas fa-star" aria-hidden="true"></i>
                                             </a>
                                         </li>
                                         <li role="presentation" data-toggle="tooltip" title="" class="tab-separator" data-original-title="<?php echo _l('mailbox_mark_as_important'); ?>">
@@ -117,7 +117,7 @@
                                         </li>
                                         <li role="presentation" data-toggle="tooltip" title="" class="tab-separator" data-original-title="<?php echo _l('mailbox_mark_as_not_important'); ?>">
                                             <a href="Javascript:void(0)" aria-controls="tab_emails_tracking" role="tab" data-toggle="tab" onclick="update_mass('<?php echo $group; ?>','important',1);window.location.reload(); return false;">
-                                                <i class="fa fa-bookmark-o" aria-hidden="true"></i>
+                                                <i class="fas fa-bookmark" aria-hidden="true"></i>
                                             </a>
                                         </li>                                        
                                         <li role="presentation" data-toggle="tooltip" title="" class="tab-separator" data-original-title="<?php echo _l('mailbox_mark_as_unread'); ?>">
@@ -127,7 +127,7 @@
                                         </li>
                                         <li role="presentation" data-toggle="tooltip" title="" class="tab-separator" data-original-title="<?php echo _l('mailbox_mark_as_read'); ?>">
                                             <a href="Javascript:void(0)" aria-controls="tab_emails_tracking" role="tab" data-toggle="tab" onclick="update_mass('<?php echo $group; ?>','read',0);window.location.reload(); return false;">
-                                                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                                <i class="fas fa-envelope" aria-hidden="true"></i>
                                             </a>
                                         </li>
                                         <?php } ?>
@@ -137,7 +137,7 @@
                                 } else {
                                     echo 'inbox';
                                 } ?>');window.location.reload(); return false;">
-                                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                                <i class="fas fa-trash" aria-hidden="true"></i>
                                             </a>
                                         </li>
                                         <?php if ('detail' == $group) {?>
